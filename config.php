9e4677bb46d061afce49d32de56ad0fa17cce50a
nirob7076 <ssb43550@gmail.com>
2026-06-18 09:13:21 +0600

    Create config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+$config = [
+    'bot_token' => '8853405908:AAGpKYpj9rFc4qYgkJMUcppd0u9nwRNUHrw', // BotFather থেকে পাওয়া টোকেন
+    'firebase_url' => 'https://aoto-2daec-default-rtdb.firebaseio.com', // ফায়ারবেস লিংক
+    'admin_id' => '8357251736', // আপনার এডমিন আইডি
+    'bot_name' => 'Super Earning Bot',
+    'main_channel_username' => '@TE1E_CASH', // চ্যানেলের ইউজারনেম (বটকে অবশ্যই চ্যানেলের এডমিন বানাতে হবে)
+    'main_channel_url' => 'https://t.me/TE1E_CASH',
+    'refer_bonus' => 10, // রেফার বোনাস
+    'min_withdraw' => 50 // মিনিমাম উইথড্র
+];
+?>
